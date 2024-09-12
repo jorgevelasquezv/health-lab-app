99b690ce06d555aecaf51cd4589d4eae9fbf1fb3
jorgevelasquezv <jorgeluisvelasquezv@gmail.com>
2024-09-11 21:16:53 -0500

    FEATURE: Modulo de citas implementado, solo validación de campos y llenado reactivo de campos con datos de usuario, no se almacena en API las citas

--- src/app/appointments/appointments.component.ts
+++ src/app/appointments/appointments.component.ts
@@ -1,25 +1,105 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 import Swal from 'sweetalert2';
+import { UserService } from '../auth/services/user.service';
+import { User } from '../domain/models/user.model';
 
 @Component({
   selector: 'appointments',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './appointments.component.html',
   styleUrl: './appointments.component.css',
 })
 export default class AppointmentsComponent {
+  private formBuilder: FormBuilder = inject(FormBuilder);
+  private userService: UserService = inject(UserService);
+  private user: User | undefined = this.userService.user();
 
-  makeAppointment() {
+  public appointmentForm = this.formBuilder.group({
+    name: [this.userFullName, [Validators.required, Validators.minLength(3)]],
+    email: [this.userEmail, [Validators.required, Validators.email]],
+    date: ['', [Validators.required, Validators.minLength(5)]],
+    time: ['', [Validators.required, Validators.minLength(5)]],
+    appointmentfor: ['', [Validators.required]],
+  });
+
+  public get userFullName() {
+    return this.user ? `${this.user.name} ${this.user.lastName}` : '';
+  }
+
+  public get userEmail() {
+    return this.user ? this.user.email : '';
+  }
+
+  public get name() {
+    return this.appointmentForm.get('name');
+  }
+
+  public get email() {
+    return this.appointmentForm.get('email');
+  }
+
+  public get date() {
+    return this.appointmentForm.get('date');
+  }
+
+  public get time() {
+    return this.appointmentForm.get('time');
+  }
+
+  public get appointmentfor() {
+    return this.appointmentForm.get('appointmentfor');
+  }
+
+  get confirmNameInvalid() {
+    if (!this.name) return false;
+    return this.name.invalid && this.name.touched;
+  }
+
+  get confirmEmailInvalid() {
+    if (!this.email) return false;
+    return this.email.invalid && this.email.touched;
+  }
+
+  get confirmDateInvalid() {
+    if (!this.date) return false;
+    return this.date.invalid && this.date.touched;
+  }
+
+  get confirmTimeInvalid() {
+    if (!this.time) return false;
+    return this.time.invalid && this.time.touched;
+  }
+
+  get confirmAppointmentforInvalid() {
+    if (!this.appointmentfor) return false;
+    return this.appointmentfor.invalid && this.appointmentfor.touched;
+  }
+
+  public makeAppointment() {
+    if (this.appointmentForm.invalid) {
+      return Object.values(this.appointmentForm.controls).forEach((control) => {
+        control.markAsTouched();
+      });
+    }
+
     Swal.fire({
-      title: 'Appointment Request Sent!',
-      text: 'You will receive a confirmation email shortly.',
+      title: '¡Solicitud de cita enviada!',
+      text: 'Recibirás un correo electrónico de confirmación en breve.',
       icon: 'success',
       confirmButtonText: 'OK',
     });
+
+    this.rechargeForm();
   }
 
+  private rechargeForm() {
+    this.appointmentForm.reset();
+    this.appointmentForm.patchValue({
+      name: this.userFullName,
+      email: this.userEmail,
+    });
+  }
 }
--- src/app/shared/top-bar/top-bar.component.ts
+++ src/app/shared/top-bar/top-bar.component.ts
@@ -1,13 +1,36 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../auth/services/auth.service';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'shared-top-bar',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule],
   templateUrl: './top-bar.component.html',
   styleUrl: './top-bar.component.css',
 })
-export class TopBarComponent { }
+export class TopBarComponent {
+  private authService: AuthService = inject(AuthService);
+  private router: Router = inject(Router);
+
+  public verifyAuth() {
+    if (this.authService.authStatus() !== 'authenticated') {
+      Swal.fire({
+        title: '¡Inicia sesión para acceder a esta funcionalidad!',
+        confirmButtonText: 'Iniciar sesión',
+        showCancelButton: true,
+        cancelButtonText: 'Cancelar',
+        cancelButtonColor: '#d33',
+        confirmButtonColor: '#00cb86',
+      }).then((result) => {
+        if (result.isConfirmed) {
+          this.router.navigate(['/login']);
+        }
+      });
+      return;
+    }
+    this.router.navigate(['/appointment']);
+  }
+}
